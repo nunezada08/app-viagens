d9c37edd20c75b37955d5d12f35561e614fefae3
nunezada08 <vitor.nunes3013@gmail.com>
2026-04-17 09:28:17 -0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,57 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={styles.container}>
+          <View style={styles.header}>
+              <View style={styles.headerView}>
+                  <FontAwesome
+                      style={styles.menuHeader}
+                      name="align-justify"
+                      size={24}
+                      color="#00BCD9"
+                  />
+                  <FontAwesome
+                      style={styles.pesquisaHeader}
+                      name="search"
+                      size={24}
+                      color="#00BCD9"
+                  />
+              </View>
+          </View>
+          <StatusBar style="auto" />
+      </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#0B121A',
+        alignItems: 'center',
+    },
+    header: {
+        width: '100%',
+        height: 92,
+        backgroundColor: '#0f0f0f',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    headerView: {
+        width: '100%',
+        height: 62,
+        flexDirection: 'row',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+    },
+    menuHeader: {
+        marginLeft: 40,
+    },
+    pesquisaHeader: {
+        marginRight: 40,
+    },
 });
